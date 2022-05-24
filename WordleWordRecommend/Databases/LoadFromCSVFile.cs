028b3c0b40abc1c803cdcd7603bea0f5e30377ce
Kurtis17 <BitsuEU@gmail.com>
2022-05-24 13:47:35 +0100

    - Added a CSV containing all 5 letter words (English) - Created a cs file that can load words from the CSV file into a list - Created a word object to store the words from CSV file into

---
+++ WordleWordRecommend/Databases/LoadFromCSVFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WordleWordRecommend.Models;
+using System.Windows;
+
+namespace WordleWordRecommend.Databases
+{
+    /// <summary>
+    /// class used to load and get data from the CSV files
+    /// </summary>
+    public class LoadFromCSVFile
+    {
+        public string ErrorCode { get; set; }
+        public List<Word> Words { get; set; }
+
+        /// <summary>
+        /// when the class is initiated, all public variables within the class are set to empty string and new List
+        /// </summary>
+        public LoadFromCSVFile()
+        {
+            ErrorCode = string.Empty;
+            Words = new List<Word>();
+        }
+
+        /// <summary>
+        /// reads all the data within the textwords file and stores them as an word object within the words list variable
+        /// </summary>
+        /// <returns></returns>
+        public bool FromCSVTextWord()
+        {
+            try
+            {
+                var info = File.ReadAllLines("listOfWords.csv");
+                foreach (string value in info)
+                {
+                    var line = value.Split(',');
+                    Words.Add(new Word()
+                    {
+                        SingleWord = line[0]
+                    });
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                ErrorCode = ex.ToString();
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// returns the list of words
+        /// </summary>
+        /// <returns></returns>
+        public List<Word> GetWords()
+        {
+            return Words;
+        }
+    }
+}
---
+++ WordleWordRecommend/Models/Word.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleWordRecommend.Models
+{
+    /// <summary>
+    ///  This class is used to be an object which stores the five leeter words from the CSV file
+    /// </summary>
+    public class Word
+    {
+        public string SingleWord { get; set; }
+
+        /// <summary>
+        /// when the class is initiated, SingleWord is set to an empty string
+        /// </summary>
+        public Word()
+        {
+            SingleWord = string.Empty;
+        }
+
+        /// <summary>
+        /// returns SingleWord
+        /// </summary>
+        /// <returns></returns>
+        public string GetWord()
+        {
+            return this.SingleWord;
+        }
+
+    }
+}
