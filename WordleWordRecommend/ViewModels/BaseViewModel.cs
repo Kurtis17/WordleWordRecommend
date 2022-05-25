cdddbea2718a5ca92acf970794d7b0a6e65073e9
Kurtis17 <BitsuEU@gmail.com>
2022-05-25 15:37:24 +0100

    - Added a base view model file to allow sub classes to inherit methods and attributes of PropertyChanged

---
+++ WordleWordRecommend/ViewModels/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WordleWordRecommend.ViewModels
+{
+    /// <summary>
+    /// A class used to allow other classes to inherit the methods and varaibles of PropertyChanged
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Sets property changes
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
