2da0a1c09eda518e33b09244e3c47eee4a5581e4
Kurtis17 <BitsuEU@gmail.com>
2022-06-02 15:50:46 +0100

    - Created a MainWindowViewModel to display content and have controls on the MainWindow

--- WordleWordRecommend/MainWindow.xaml.cs
+++ WordleWordRecommend/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WordleWordRecommend.ViewModels;
 
 namespace WordleWordRecommend
 {
@@ -23,6 +24,8 @@ namespace WordleWordRecommend
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ WordleWordRecommend/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Controls;
+using WordleWordRecommend.Commands;
+using WordleWordRecommend.Views;
+
+namespace WordleWordRecommend.ViewModels
+{
+    /// <summary>
+    /// this class is the main window of the application, it controls the UserControls and depending on the buttons pressed the appropriate WPF will be displayed within the UserControls
+    /// </summary>
+    public class MainWindowViewModel : BaseViewModel
+    {
+        #region Button Content
+        public string PlayGameContent { get; private set; }
+        public string WordFinderContent { get; private set; }
+        #endregion
+
+        #region Button Command
+        public ICommand PlayGameCommand { get; private set; }
+        public ICommand WordFinderCommand { get; private set; }
+        #endregion
+
+        #region User Control
+        public UserControl ContentControlBindingOne { get; private set; }
+        #endregion
+
+        #region Constructor 
+        public MainWindowViewModel()
+        {
+            PlayGameContent = "New word";
+            WordFinderContent = "Find word";
+
+            PlayGameCommand = new RelayCommand(PlayGameButtonClick);
+            WordFinderCommand = new RelayCommand(WordFinderButtonClick);
+
+            
+
+        }
+        #endregion
+
+        #region Button Click
+        private void PlayGameButtonClick()
+        {
+
+        }
+        private void WordFinderButtonClick()
+        {
+
+        }
+        #endregion 
+    }
+}
